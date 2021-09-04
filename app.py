dd4e18ba477cb05337b33cafaaed08d1bd4f1976
jpkelvin <jpkelvin2001@gmail.com>
2021-09-04 13:24:41 +0530

    Image to text conversion done

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask,redirect,url_for,render_template,request
-from extracter import text
+from flask import Flask, config,redirect,url_for,render_template,request
+from extracter import extract
+from werkzeug.utils import secure_filename
+import os
 app=Flask(__name__)
-
+app.config['UPLOAD_FOLDER']='static/images/'
 @app.route('/')
 def welcome():
     return render_template('index.html')
 
 @app.route('/submit',methods=['POST','GET'])
 def output():
-    Image=''
     if request.method=='POST':
-        return Image
+        Img= request.files['img']
+        file_name=secure_filename(Img.filename)
+        Img.save(os.path.join(app.config['UPLOAD_FOLDER'],file_name))
+        text=extract(file_name)
+        return render_template('results.html',img=file_name, extracted_text=text)
 if __name__=='__main__':
     app.run(debug=True)
--- extracter.py
+++ extracter.py
@@ -3,18 +3,20 @@ import pytesseract as tess
 import os,re,send2trash
 tess.pytesseract.tesseract_cmd = r'C:\Users\hp\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-text=[]
-iamge=Image
-# path = os.path.dirname(os.path.realpath(__file__))
-# input_path = path + '/sampleimages/'
-# #opening files and extracting text
-# for root,dirs,filenames in os.walk(input_path):
-#     for filename in filenames:
-try:
-    img = Image.open(Image)
-    text.append(tess.image_to_string(img))
-    send2trash.send2trash(Image)
-except:
-    print("FInvalid File")
-text = str('\n'.join(text))
-print(text)
+path = os.path.dirname(os.path.realpath(__file__))
+input_path = path + '/static/images/'
+#opening files and extracting text
+
+
+def extract(img):
+    text=[]
+    for root,dirs,filenames in os.walk(input_path):
+        try:
+            img1 = Image.open(input_path + img)
+            text.append(tess.image_to_string(img1))
+            send2trash.send2trash(input_path+img)
+        except:
+            # return 'Invalid Image'
+            continue
+    text = str('\n'.join(text))
+    return text
